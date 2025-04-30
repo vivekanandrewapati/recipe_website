307f5594191bfcbc4a2d579c3ef15dc9e35c8847
harry <rewapativivekanand@gmail.com>
2025-04-30 16:57:40 +0530

    updated gemini version

--- src/pages/MoodBased.jsx
+++ src/pages/MoodBased.jsx
@@ -23,7 +23,7 @@ const Moodbased = () => {
     setError('');
 
     try {
-      const response = await fetch(`https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent?key=${API_KEY}`, {
+      const response = await fetch(`https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=${API_KEY}`, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
